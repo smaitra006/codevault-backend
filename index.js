86522f0ea35e701a7e33449afa738d52cd801e93
Subhrajit Maitra <smaitra006@gmail.com>
2025-10-05 14:22:04 +0530

    feat: Create basic HTTP server with routing for snippets

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
+const http = require("http");
+const PORT = 5000;
 const appName = "CodeVault";
 
-const firstSnippet = {
-  id: 1,
-  title: "Simple Express Server",
-  language: "Javascript",
-  code: "const express = require('express');\nconst app = express();\n\napp.listen(3000, () => console.log('Server runiing));",
-  tags: ["node.js", "express", "server"],
-};
+const snippets = [
+  { id: 1, language: "JavaScript", code: 'console.log("Hello, World!");' },
+  { id: 2, language: "Python", code: 'print("Hello, World!")' },
+];
 
-const fetchSnippet = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("Snippet fetched from database!");
-      resolve(firstSnippet);
-    }, 1500);
-  });
-};
-
-const displaySnippet = (snippetObject) => {
-  console.log("--- Snippet Details ---");
-  console.log(`ID: ${snippetObject.id}`);
-  console.log(`Title: ${snippetObject.title}`);
-  console.log(`Language: ${snippetObject.language}`);
-  console.log(`Code: \n${snippetObject.code}`);
-
-  console.log(`Tags: ${snippetObject.tags.join(", ")}`);
-  console.log("-----------------------");
-};
-
-const startApp = async () => {
-  try {
-    console.log(`Welcome to ${appName}`);
-    const snippet = await fetchSnippet();
-    displaySnippet(snippet);
-  } catch (error) {
-    console.log("Failed to start the application:", error);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to the CodeVault API!");
+  } else if (req.url === "/api/snippets") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(snippets));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
   }
-};
+});
 
-startApp();
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
