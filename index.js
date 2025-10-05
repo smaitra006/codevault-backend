07e8e04941b98e839e351bf60c9243a69eaa8b2d
Subhrajit Maitra <smaitra006@gmail.com>
2025-10-05 15:17:40 +0530

    refactor: Convert core HTTP server to an Express app

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const http = require("http");
+const express = require("express");
+
+const app = express();
+
 const PORT = 5000;
-const appName = "CodeVault";
 
 const snippets = [
   { id: 1, language: "JavaScript", code: 'console.log("Hello, World!");' },
   { id: 2, language: "Python", code: 'print("Hello, World!")' },
 ];
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to the CodeVault API!");
-  } else if (req.url === "/api/snippets") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(snippets));
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
-  }
+app.get("/", (req, res) => {
+  res.send("Welcome to the CodeVault API! Made with Express.");
 });
 
-server.listen(PORT, () => {
+app.get("/api/snippets", (req, res) => {
+  res.json(snippets);
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
